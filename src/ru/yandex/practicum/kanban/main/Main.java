3c50d6033fb7beb4e8b41c41b523b79c800a605e
dilyana.motina <d.motina>
2022-09-14 15:47:15 +0300

    refactor: Вынести Main.java в отдельный пакет, код TestCases вынести из основного метода в приватный

--- src/ru/yandex/practicum/kanban/main/Main.java
+++ src/ru/yandex/practicum/kanban/main/Main.java
@@ -1,6 +1,15 @@
+package ru.yandex.practicum.kanban.main;
+
+import ru.yandex.practicum.kanban.task.*;
+import ru.yandex.practicum.kanban.manager.TaskManager;
+
 public class Main {
 
     public static void main(String[] args) {
+        printTestCases();
+    }
+
+    private static void printTestCases() {
         System.out.println("Поехали!");
         TaskManager manager = new TaskManager();
         //Создаем две задачи
@@ -19,15 +28,17 @@ public class Main {
         System.out.println(manager.getTasks());
         System.out.println(manager.getSubTasks());
 
-        manager.update(subTask1.getUuid(), new SubTask("nameUpd", "descUpd", "NEW", subTask1.getEpicUuid(), subTask1.getUuid()));
-        manager.update(subTask2.getUuid(), new SubTask("nameUpd2", "descUpd2", "DONE", subTask2.getEpicUuid(), subTask2.getUuid()));
-        manager.update(subTask3.getUuid(), new SubTask("nameUpd3", "descUpd3", "DONE", subTask3.getEpicUuid(), subTask3.getUuid()));
+        //Обновляем subTasks. Проверяем изменение name, description и status
+        manager.update(subTask1.getUuid(), new SubTask(subTask1.getUuid(), "nameUpd", "descUpd", "NEW", subTask1.getEpicUuid()));
+        manager.update(subTask2.getUuid(), new SubTask(subTask2.getUuid(), "nameUpd2", "descUpd2", "DONE", subTask2.getEpicUuid()));
+        manager.update(subTask3.getUuid(), new SubTask(subTask3.getUuid(), "nameUpd3", "descUpd3", "DONE", subTask3.getEpicUuid()));
         manager.update(task1.getUuid(), new Task(task1.getUuid(), "nameUpd3", "descUpd3", "DONE"));
 
         System.out.println(manager.getEpics());
         System.out.println(manager.getTasks());
         System.out.println(manager.getSubTasks());
 
+        //Удаляем одну из двух задач в эпике и полность один эпик
         manager.deleteSubTask(subTask1.getUuid(), subTask1.getEpicUuid());
         manager.deleteEpic(epic2.getUuid());
 
