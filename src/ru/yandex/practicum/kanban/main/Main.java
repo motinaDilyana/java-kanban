824d8a0a66fe5f381ceb648b5e8eeb717928bdcd
d.motina <d.motina@ixr.com>
2022-11-22 20:40:50 +0300

    перенесла тесты с эпиком в другой класс

--- src/ru/yandex/practicum/kanban/main/Main.java
+++ src/ru/yandex/practicum/kanban/main/Main.java
@@ -1,16 +1,6 @@
 package ru.yandex.practicum.kanban.main;
-
-import ru.yandex.practicum.kanban.manager.FileBackendTaskManager;
-import ru.yandex.practicum.kanban.manager.Managers;
-import ru.yandex.practicum.kanban.manager.TaskManager;
 import ru.yandex.practicum.kanban.manager.exceptions.NullTaskException;
 import ru.yandex.practicum.kanban.manager.exceptions.TaskNotFoundException;
-import ru.yandex.practicum.kanban.task.*;
-import ru.yandex.practicum.kanban.task.model.TaskDates;
-
-import java.io.File;
-import java.time.LocalDateTime;
-import java.time.Month;
 
 public class Main {
 
